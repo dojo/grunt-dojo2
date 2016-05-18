23dfc55e028acf0ceb83e8d682d7e8a12caa822b
Kitson Kelly <me@kitsonkelly.com>
2016-05-18 09:26:35 +0100

    Remove /typings from git

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/gruntjs/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/glob/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/gruntjs/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/definitions/glob/index.d.ts" />
