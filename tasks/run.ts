339b724e4ee3685dcb00c00b45d1f73207161a6b
Anthony Gubler <agubler@sitepen.com>
2016-08-04 11:40:53 +0100

    cast grunt option to string

--- tasks/run.ts
+++ tasks/run.ts
@@ -4,7 +4,7 @@ export = function(grunt: IGrunt, packageJson: any) {
 	grunt.registerTask('run', 'Bootstrap dojo-loader and run the given --main', function () {
 		this.async(); // Ensure Grunt doesn't exit the process.
 
-		const main = grunt.option('main') || 'src/main';
+		const main = <string> grunt.option('main') || 'src/main';
 		grunt.log.ok(main);
 
 		const { require } = loadDojoLoader(packageJson);
