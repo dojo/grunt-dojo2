f2ef4d13476d737e71ae5267de5e415e72eff3c3
Tom Dye <github@tomdye.co.uk>
2017-01-17 18:18:30 +0000

    changed to 8 char hash

--- tasks/postcss.ts
+++ tasks/postcss.ts
@@ -22,7 +22,7 @@ export = function(grunt: IGrunt) {
 			}
 		}),
 		postCssModules({
-			generateScopedName: '[name]__[local]__[hash:base64:5]',
+			generateScopedName: '[hash:base64:8]',
 			getJSON: function(cssFileName: string, json: JSON) {
 				const outputPath = path.resolve(distDirectory, path.relative('src', cssFileName));
 				fs.writeFileSync(outputPath + '.json', JSON.stringify(json));
