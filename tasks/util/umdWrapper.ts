85b42cdd076f1bb3264e638ee5b31be27e60aae5
Tom Dye <github@tomdye.co.uk>
2017-01-26 11:53:33 +0000

    indentation

--- tasks/util/umdWrapper.ts
+++ tasks/util/umdWrapper.ts
@@ -1,13 +1,13 @@
 const umdPrefix = `(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		define([], function () { return (factory()); });
-	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory();
-	}
-	}(this, function () {
-		return `;
+if (typeof define === 'function' && define.amd) {
+	define([], function () { return (factory()); });
+} else if (typeof module === 'object' && module.exports) {
+	module.exports = factory();
+}
+}(this, function () {
+	return `;
 const umdSuffix = `;
-	}));`;
+}));`;
 
 export = function(content: string): string {
 	return `${umdPrefix}${content}${umdSuffix}`;
