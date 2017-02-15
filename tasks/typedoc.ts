f120db1c2d1d479aecd1b4d5d03b5296aa7fb0e2
Jason Cheatham <j.cheatham@gmail.com>
2017-02-15 08:32:05 -0500

    Update to use typedoc 0.5.6

--- tasks/typedoc.ts
+++ tasks/typedoc.ts
@@ -1,7 +1,5 @@
 import ITask = grunt.task.ITask;
 import { config, cp, rm } from 'shelljs';
-import { dirname, relative } from 'path';
-import { cloneDeep } from 'lodash';
 import { execSync } from 'child_process';
 import { join } from 'path';
 
@@ -60,52 +58,12 @@ export = function (grunt: IGrunt) {
 			grunt.log.writeln(`Pushed ${publishBranch} to origin`);
 		}
 
-		/**
-		 * typedoc <= 0.5.5 breaks TS's automatic type discovery, and also leaves out important compiler flags like
-		 * strictNullChecks. Fake the type discovery, and ignore remaining errors.
-		 *
-		 * TODO: Remove this when TypeDoc has been updated.
-		 */
-		function setupForOldTypedoc() {
-			const typedocInfo = grunt.file.readJSON(require.resolve('typedoc/package.json'));
-			const [ major, minor, patch ] = typedocInfo.version.split('.').map(Number);
-			if (major > 0 || minor > 5 || patch > 5) {
-				return false;
-			}
-
-			grunt.loadNpmTasks('grunt-ts');
-			const tsconfig: any = options.tsconfig ? grunt.file.readJSON(options.tsconfig) :
-				cloneDeep(grunt.config.get('tsconfig'));
-
-			const tsconfigFileName = join(dirname(options.out), '.tsconfig-typedoc.json');
-			const base = dirname(relative(dirname(tsconfigFileName), 'node_modules'));
-
-			// The type paths need to be relative to the temp tsconfig file that will be created
-			const installedTypes = grunt.file.expandMapping(['node_modules/@types/*/index.d.ts'], base, {}).map(mapping => {
-				return mapping.dest;
-			});
-			if (installedTypes.length > 0) {
-				tsconfig.include = installedTypes.concat(tsconfig.include || []);
-			}
-
-			// Put temp tsconfig file in same directory as output
-			grunt.file.write(tsconfigFileName, JSON.stringify(tsconfig, null, '  '));
-			args.push('--tsconfig', `"${tsconfigFileName}"`);
-
-			grunt.config.set('clean.typedocTsconfig', { src: tsconfigFileName});
-			grunt.task.run('clean:typedocTsconfig');
-
-			args.push('--ignoreCompilerErrors');
-
-			return true;
-		}
-
 		const options: any = this.options({});
 		options.out = options.out || grunt.config.get<string>('apiDocDirectory');
 
 		const args: string[] = [];
 		Object.keys(options).filter(key => {
-			return key !== 'publishOptions' && key !== 'tsconfig';
+			return key !== 'publishOptions';
 		}).forEach(key => {
 			if (typeof options[key] === 'boolean') {
 				if (options[key]) {
@@ -117,10 +75,6 @@ export = function (grunt: IGrunt) {
 				args.push(`"${options[key]}"`);
 			}
 		});
-
-		if (!setupForOldTypedoc() && options.tsconfig) {
-			args.push('--tsconfig', `"${options.tsconfig}"`);
-		}
 
 		// Use project-local typedoc
 		const typedoc = require.resolve('typedoc/bin/typedoc');
--- tests/unit/tasks/typedoc.ts
+++ tests/unit/tasks/typedoc.ts
@@ -22,7 +22,6 @@ let publishApi: boolean;
 let execSync: SinonSpy;
 let cp: SinonSpy;
 let rm: SinonSpy;
-let typedocVersion: string;
 let shouldPush: SinonSpy;
 let shouldPushValue: boolean;
 let failInitialCheckout: boolean;
@@ -58,14 +57,7 @@ registerSuite({
 			return [ 'foo' ];
 		});
 		readJSON = stub(grunt.file, 'readJSON', (filename: string) => {
-			if (filename.indexOf('package.json') !== -1) {
-				return {
-					version: typedocVersion
-				};
-			}
-			else {
-				return {};
-			}
+			return {};
 		});
 		shouldPush = spy(() => shouldPushValue);
 
@@ -125,33 +117,17 @@ registerSuite({
 		failInitialCheckout = false;
 	},
 
-	default: {
-		// TODO: Remove this check after updating to TypeDoc 0.5.6+
-		'old typedoc'() {
-			typedocVersion = '0.5.5';
-			runGruntTask('typedoc');
-			assert.strictEqual(shouldPush.callCount, 0, 'Push check should not have been called');
-			assert.strictEqual(write.callCount, 1, 'One file should have been written');
-			assert.strictEqual(execSync.callCount, 1, 'Unexpected number of exec calls');
-		},
-
-		'new typedoc'() {
-			typedocVersion = '0.5.6';
-			runGruntTask('typedoc');
-			assert.strictEqual(shouldPush.callCount, 0, 'Push check should not have been called');
-			assert.strictEqual(write.callCount, 0, 'Nothing should have been written');
-			assert.strictEqual(execSync.callCount, 1, 'Unexpected number of exec calls');
-		},
-
-		'typedoc command'() {
-			typedocVersion = '0.5.6';
-			runGruntTask('typedoc');
-			const command = execSync.args[0][0];
-			const matcher = new RegExp(
-				`node "[^"]+/typedoc" --mode "modules" --excludeExternals --excludeNotExported ` +
-				`--logger "none" --out "${apiDocDirectory}" --tsconfig "${join(outputPath, 'tsconfig.json')}"`);
-			assert.match(command, matcher, 'Unexpected typedoc command line');
-		}
+	default() {
+		runGruntTask('typedoc');
+		const command = execSync.args[0][0];
+		const matcher = new RegExp(
+			`node "[^"]+/typedoc" --mode "modules" --excludeExternals --excludeNotExported ` +
+			`--tsconfig "${join(outputPath, 'tsconfig.json')}" ` +
+			`--logger "none" --out "${apiDocDirectory}"`);
+		assert.match(command, matcher, 'Unexpected typedoc command line');
+		assert.strictEqual(shouldPush.callCount, 0, 'Push check should not have been called');
+		assert.strictEqual(write.callCount, 0, 'Nothing should have been written');
+		assert.strictEqual(execSync.callCount, 1, 'Unexpected number of exec calls');
 	},
 
 	publish: (() => {
@@ -161,7 +137,6 @@ registerSuite({
 			setup() {
 				publishApi = grunt.option<boolean>('publish-api');
 				grunt.option('publish-api', true);
-				typedocVersion = '0.5.6';
 			},
 
 			teardown() {
