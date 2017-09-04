6c4e4bde87cdbb4b7c3378442f6da107076a5bea
Anthony Gubler <agubler@gmail.com>
2017-09-04 10:55:42 +0100

    concatenate all the dependencies in a single npm install command (#154)

--- tests/unit/tasks/installPeerDependencies.ts
+++ tests/unit/tasks/installPeerDependencies.ts
@@ -5,24 +5,46 @@ import { SinonStub, stub } from 'sinon';
 import { loadTasks, unloadTasks, runGruntTask } from '../util';
 
 let mockLogger: SinonStub;
+let mockErrorLogger: SinonStub;
 let mockShell: SinonStub;
 
 registerSuite({
 	name: 'tasks/installPeerDependencies',
 	afterEach() {
+		mockShell.reset();
 		mockLogger.restore();
+		mockErrorLogger.restore();
 		unloadTasks();
 	},
 	'npm': {
 		beforeEach() {
 			grunt.initConfig({});
-			mockLogger = stub(grunt.log, 'error');
+			mockErrorLogger = stub(grunt.log, 'error');
+			mockLogger = stub(grunt.log, 'write');
 			mockShell = stub();
 
 			mockShell
 			.withArgs('npm install my-dep@"1.0" --no-save').returns(Promise.resolve({}))
-			.withArgs('npm install error-dep@"1.0" --no-save').throws();
+			.withArgs('npm install my-dep@"1.0" error-dep@"1.0" --no-save').throws();
+		},
+		'sucessfully install peer dependencies'() {
+			loadTasks({
+				child_process: {
+					execSync: mockShell
+				}
+			}, {
+				peerDependencies: {
+					'my-dep': '1.0'
+				}
+			});
+			runGruntTask('peerDepInstall');
 
+			assert.isTrue(mockShell.calledOnce);
+			assert.isTrue(mockShell.calledWith('npm install my-dep@"1.0" --no-save'));
+			assert.isTrue(mockLogger.calledOnce);
+			assert.isTrue(mockErrorLogger.notCalled);
+		},
+		'fail to install peer dependencies'() {
 			loadTasks({
 				child_process: {
 					execSync: mockShell
@@ -33,15 +55,27 @@ registerSuite({
 					'error-dep': '1.0'
 				}
 			});
-		},
-		runsCommands() {
 			runGruntTask('peerDepInstall');
 
-			assert.isTrue(mockShell.calledTwice);
-			assert.isTrue(mockShell.calledWith('npm install my-dep@"1.0" --no-save'));
-			assert.isTrue(mockShell.calledWith('npm install error-dep@"1.0" --no-save'));
-			assert.isTrue(mockLogger.called);
-			assert.isTrue(mockLogger.calledWith('failed.'));
+			assert.isTrue(mockShell.calledOnce);
+			assert.isTrue(mockShell.calledWith('npm install my-dep@"1.0" error-dep@"1.0" --no-save'));
+			assert.isTrue(mockLogger.calledOnce);
+			assert.isTrue(mockErrorLogger.calledWith('failed.'));
+		},
+		'no peer deps found'() {
+			loadTasks({
+				child_process: {
+					execSync: mockShell
+				}
+			}, {
+				peerDependencies: {
+				}
+			});
+			runGruntTask('peerDepInstall');
+
+			assert.isTrue(mockShell.notCalled);
+			assert.isTrue(mockLogger.calledWith('No peer dependencies detected.'));
+			assert.isTrue(mockErrorLogger.notCalled);
 		}
 	}
 });
