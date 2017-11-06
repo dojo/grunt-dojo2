8054d26de5a120c312364f97acfb55b0a8fc6da3
Rory Mulligan <rmulligan@sitepen.com>
2017-11-06 09:37:34 -0500

    Adding tests for intern loader

--- lib/intern/internLoader.ts
+++ lib/intern/internLoader.ts
@@ -1,7 +1,5 @@
 declare const shimAmdDependencies: any;
 
-// this file is loaded directly by intern, thus coverage is not available.
-/* istanbul ignore next */
 intern.registerLoader((options) => {
 	return intern.loadScript('node_modules/@dojo/loader/loader.js')
 		.then(() => intern.loadScript('node_modules/@dojo/shim/util/amd.js'))
