a033cb496c9a919d28a9e6d03086feafeef35e0c
Bryan Forbes <bryan@reigndropsfall.net>
2017-11-09 12:20:06 -0600

    Update to Intern 4.1.0 and TypeScript 2.6.1 and turn on strict (#172)

--- options/intern.ts
+++ options/intern.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import { parseJson } from 'intern/lib/common/util';
 
 export = function (grunt: IGrunt) {
 	grunt.loadNpmTasks('intern');
@@ -6,14 +7,20 @@ export = function (grunt: IGrunt) {
 	const progress = grunt.option<boolean>('progress');
 
 	// read the test config and find a loader. if no loader is specified, we need to add ours
-	const { browser: { loader: browserLoader = undefined } = {}, loader } = grunt.file.readJSON(path.resolve(grunt.config.get('internConfig')));
+	const internJson = grunt.file.read(path.resolve(grunt.config.get('internConfig')));
+	const { browser: { loader: browserLoader = undefined } = {}, loader } = parseJson(internJson);
 
 	return {
 		options: {
 			config: '<%= internConfig %>',
-			reporters: [
-				{ name: 'runner', options: { 'hideSkipped': !progress, 'hidePassed': !progress } }
-			],
+			node: {
+				'plugins+': [
+					{ script: 'grunt-dojo2/lib/intern/Reporter.js', useLoader: true }
+				],
+				reporters: [
+					{ name: 'runner', options: { 'hideSkipped': !progress, 'hidePassed': !progress } }
+				]
+			},
 			browser: (loader || browserLoader) ? {} : {
 				loader: './node_modules/grunt-dojo2/lib/intern/internLoader.js'
 			}
