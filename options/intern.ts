632327c4c7264baf68006b72742b808c032aa449
Bryan Forbes <bryan@reigndropsfall.net>
2017-11-14 12:06:52 -0600

    Remove code setting browser loader in-lieu of packages adding it themselves (#176)

--- options/intern.ts
+++ options/intern.ts
@@ -1,14 +1,9 @@
 import * as path from 'path';
-import { parseJson } from 'intern/lib/common/util';
 
 export = function (grunt: IGrunt) {
 	grunt.loadNpmTasks('intern');
 
 	const progress = grunt.option<boolean>('progress');
-
-	// read the test config and find a loader. if no loader is specified, we need to add ours
-	const internJson = grunt.file.read(path.resolve(grunt.config.get('internConfig')));
-	const { browser: { loader: browserLoader = undefined } = {}, loader } = parseJson(internJson);
 
 	return {
 		options: {
@@ -20,9 +15,6 @@ export = function (grunt: IGrunt) {
 				reporters: [
 					{ name: 'runner', options: { 'hideSkipped': !progress, 'hidePassed': !progress } }
 				]
-			},
-			browser: (loader || browserLoader) ? {} : {
-				loader: './node_modules/grunt-dojo2/lib/intern/internLoader.js'
 			}
 		},
 		browserstack: {
